c13704478625cb6de562f5b9274f981a468c605d
David Souther <davidsouther@gmail.com>
2014-11-11 17:08:29 -0500

    Prioritize config.

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ module.exports = function (config){
 
     config.vendors = config.vendors || {};
     config.vendors.prefix = config.vendors.prefix || [];
-    config.vendors.prefix.push(Path.resolve(__dirname, '../node_modules'));
+    config.vendors.prefix.unshift(Path.resolve(__dirname, '../node_modules'));
 
     config.vendors.js = (config.vendors.js || []).concat([
         'jquery/dist/jquery.js',
